e32e55f60b5a968e04c28a729ef1c394dea494c5
ymadd <Kb_yamato@protonmail.com>
2021-07-01 02:31:50 +0900

    create game window

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame
 
 pygame.init()
+width = 800
+heigh = 800
+white_color = (255, 255, 255)
+
+game_window = pygame.display.set_mode((width, heigh))
+game_window.fill(white_color)
+pygame.display.update()
+
 pygame.quit()
 quit()
