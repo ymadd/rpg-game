3bd08464c296fcdbfb4db3d9d99f965e1d507565
ymadd <Kb_yamato@protonmail.com>
2021-07-01 02:44:47 +0900

    make loop

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ heigh = 800
 white_color = (255, 255, 255)
 
 game_window = pygame.display.set_mode((width, heigh))
-game_window.fill(white_color)
-pygame.display.update()
+
+clock = pygame.time.Clock()
+
+while True:
+    # Handle events
+    # Excute logic
+    # Update display
+    game_window.fill(white_color)
+    pygame.display.update()
+
+    clock.tick(60)
 
 pygame.quit()
 quit()
