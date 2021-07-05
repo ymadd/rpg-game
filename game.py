a85cf71bb87abcb92bb4d7617ad291ac03adedc9
ymadd <Kb_yamato@protonmail.com>
2021-07-05 18:40:20 +0900

    add gameObjects.py

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from gameObject import GameObject
 
 
 class Game:
@@ -9,21 +10,19 @@ class Game:
 
         self.game_window = pygame.display.set_mode((self.width, self.heigh))
 
-        background_image = pygame.image.load("assets/background.png")
-        self.background_image = pygame.transform.scale(
-                background_image, (self.width, self.heigh))
-
-        tresure_image = pygame.image.load("assets/treasure.png")
-        self.tresure_image = pygame.transform.scale(tresure_image, (50, 50))
+        self.background = GameObject(
+            0, 0, self.width, self.heigh, 'assets/background.png')
+        self.tresure = GameObject(375, 50, 50, 50, 'assets/treasure.png')
 
         self.clock = pygame.time.Clock()
 
     def draw_object(self):
         self.game_window.fill(self.white_color)
 
-        self.game_window.blit(self.background_image, (0, 0))
-        self.game_window.blit(self.tresure_image, (375, 50))
-
+        self.game_window.blit(
+            self.background.image, (self.background.x, self.background.y))
+        self.game_window.blit(
+            self.tresure.image, (self.tresure.x, self.tresure.y))
         pygame.display.update()
 
     def run_game_loop(self):
