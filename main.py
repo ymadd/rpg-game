a94e4e840c3a4660e93e12a3bf3199f42bd2b9b8
ymadd <Kb_yamato@protonmail.com>
2021-07-05 17:47:44 +0900

    add game background image

--- main.py
+++ main.py
@@ -9,14 +9,26 @@ game_window = pygame.display.set_mode((width, heigh))
 
 clock = pygame.time.Clock()
 
+background_image = pygame.image.load("assets/background.png")
+background_image = pygame.transform.scale(background_image, (width, heigh))
+
+tresure_image = pygame.image.load("assets/treasure.png")
+tresure_image = pygame.transform.scale(tresure_image, (50, 50))
+
 
 def run_game_loop():
     while True:
+        # Handle events
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 return
+
+        # Excute game logic
+        # Update display
         game_window.fill(white_color)
+        game_window.blit(background_image, (0, 0))
+        game_window.blit(tresure_image, (375, 50))
         pygame.display.update()
 
         clock.tick(60)
